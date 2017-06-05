f2c5915253946d53d7e15c5bad944e38e7a8fe96
leungant <leungant@a.com>
2017-06-05 15:13:03 +0200

    version number, desc tweak

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='asyncd',
-      version='1.21',
-      description='Cool yet simple async in Python 3 and 2',
+      version='1.35',
+      description='Cool yet simple async for Python 3 and 2',
       url='http://github.com/leungant/asyncd',
       author='Anthony Leung',
       author_email='anthony.leung@gmail.gm',

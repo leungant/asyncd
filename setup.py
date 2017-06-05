720b2ba899ed88fbeb917973007693acf24473ba
leungant <leungant@a.com>
2017-06-05 15:19:15 +0200

    packaging

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
 setup(name='asyncd',
-      version='1.35',
+      version='1.37',
       description='Cool yet simple async for Python 3 and 2',
       url='http://github.com/leungant/asyncd',
       author='Anthony Leung',
       author_email='anthony.leung@gmail.gm',
       license='MIT',
       packages=['asyncd'],
+      install_requires=[
+          'pylru',
+      ],
       zip_safe=False)
